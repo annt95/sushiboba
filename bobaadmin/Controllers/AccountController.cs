0aa6956f7794be4f53794cedafaba6d98d63345f
An-Cty <ngothanhan95@gmail.com>
2020-03-04 18:54:02 +0700

    update

--- bobaadmin/Controllers/AccountController.cs
+++ bobaadmin/Controllers/AccountController.cs
@@ -5,35 +5,107 @@ using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.V4.Pages.Account.Internal;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.WebUtilities;
+using System.Text;
+using Microsoft.AspNetCore.Authentication;
+using System.Security.Claims;
+
 namespace bobaadmin
 {
+    [AllowAnonymous]
     public class AccountController : Controller
     {
+        private readonly ILogger<LogoutModel> _logger;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
         MyDbContext db = new MyDbContext();
         public IActionResult Login()
         {
             ViewBag.Message = HttpContext.Session.GetString("username");
             return View();
         }
-        public ActionResult Validate(Admins admin)
-        {
-            var _admin = db.Admins.Where(s => s.Email == admin.Email);
-            if (_admin.Any())
-            {
-                if (_admin.Where(s => s.Password == admin.Password).Any())
-                {
+        [BindProperty]
+        public InputModel Input { get; set; }
 
+        public IList<AuthenticationScheme> ExternalLogins { get; set; }
+
+        public string ReturnUrl { get; set; }
+
+        [TempData]
+        public string ErrorMessage { get; set; }
+
+        public class InputModel
+        {
+            [Required]
+            [EmailAddress]
+            public string Email { get; set; }
+
+            [Required]
+            [DataType(DataType.Password)]
+            public string Password { get; set; }
+
+            [Display(Name = "Remember me?")]
+            public bool RememberMe { get; set; }
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        public async Task<IActionResult> Validate(Admins admin)
+        {
+           
+            if (ModelState.IsValid)
+            {
+                // This doesn't count login failures towards account lockout
+                // To enable password failures to trigger account lockout, 
+                // set lockoutOnFailure: true
+                var result = await _signInManager.PasswordSignInAsync(admin.Email,
+                                   admin.Password, true, lockoutOnFailure: true);
+                if (result.Succeeded)
+                {
+                    var principal = new ClaimsPrincipal(new ClaimsIdentity( "admin"));
+                    HttpContext.Authentication.SignInAsync("admin", principal);
+                    _logger.LogInformation("User logged in.");
                     return Json(new { status = true, message = "Login Successfull!" });
                 }
+                
                 else
                 {
+                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
                     return Json(new { status = false, message = "Invalid Password!" });
                 }
             }
             else
             {
-                return Json(new { status = false, message = "Invalid User!" });
+                ModelState.AddModelError("", "Invalid login attempt.");
+                return Json(new { status = false, message = "Invalid login attempt." });
             }
+
+
+
+
+
+            //var _admin = db.Admins.Where(s => s.Email == admin.Email);
+            //if (_admin.Any())
+            //{
+            //    if (_admin.Where(s => s.Password == admin.Password).Any())
+            //    {
+
+            //        return Json(new { status = true, message = "Login Successfull!" });
+            //    }
+            //    else
+            //    {
+            //        return Json(new { status = false, message = "Invalid Password!" });
+            //    }
+            //}
+            //else
+            //{
+            //    return Json(new { status = false, message = "Invalid User!" });
+            //}
         }
         public ActionResult checkLogin(Admins admin)
         {
--- bobaadmin/Startup.cs
+++ bobaadmin/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -13,12 +14,17 @@ namespace bobaadmin
     {
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDistributedMemoryCache();
-
-            services.AddSession(options => {
-                options.IdleTimeout = TimeSpan.FromMinutes(300);//You can set Time
+            services.AddAuthentication(options =>
+            {
+                options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            }).AddCookie(options => { options.LoginPath = "/Login"; });
+            services.AddMvc().AddRazorPagesOptions(options =>
+            {
+                options.Conventions.AuthorizeFolder("/");
+                options.Conventions.AllowAnonymousToPage("/Login");
             });
-            services.AddMvc();
         }
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
